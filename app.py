3478a94668db7121813a077e16da4e39068f65a5
3rdSon <nnajivictorious@gmail.com>
2024-12-10 15:51:28 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+
+# Set page config
+st.set_page_config(page_title="Hire Me - Victory", page_icon="🎉", layout="centered")
+
+# Display balloons
+st.balloons()
+
+# Title
+st.title("😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂")
+st.title("You Think Say I Wan Talk My Story?, Person wey no get money dy give advise?")
+# Main content
+st.markdown(
+    """
+    ## Abeg, na my last card I use buy this thing so...  
+    ### **Please Give Me Jobs 🙏 Make I see food chop for this December**  
+    I am:
+    - A **Data Analyst**
+    - A **Data Scientist**
+    - An **AI Engineer**
+    - A **Machine Learning Engineer**
+    - A **Python Developer**
+    
+    With **3 years of experience**, I promise you won't regret it.  
+
+    ### **Just try me and see how good I am at this job.**  
+    ---
+    ### **Links to Showcase My Work:**
+    - 📝 [Medium Articles](https://medium.com/@3rdSon)
+    - 🐙 [GitHub](https://github.com/3rd-Son)
+    - 💼 [LinkedIn](https://www.linkedin.com/in/3rdson/)
+
+    ---
+    ### You Just Have to Trust Me
+    I **deliver** like Hebrew women. Don't wait. **Reach out now! I'm too good😭!!!**
+    """,
+    unsafe_allow_html=True,
+)
+
+# Footer
+st.markdown(
+    """
+    <style>
+    .footer {
+        font-size: 1.2rem;
+        margin-top: 3rem;
+        text-align: center;
+        font-weight: bold;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    '<div class="footer">💪 As you can see, I built this with Python, that is how good and versatile I am 💪</div>',
+    unsafe_allow_html=True,
+)
